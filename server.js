6e89bc9c95b0b146d047f90cd6537fc690e45d3a
Michael Aranda <michaelaranda0.web@gmail.com>
2018-07-16 19:46:54 +0000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,20 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Server listening at", addr.address + ":" + addr.port);
+});
