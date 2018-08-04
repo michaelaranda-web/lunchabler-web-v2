d62215fa55e6e76de531255acc178498b3ae7874
Michael Aranda <michaelaranda0.web@gmail.com>
2018-08-04 01:17:00 +0000

    Adds Redux Chrome DevTools integration.

--- client/js/index.js
+++ client/js/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware  } from 'redux';
+import { createStore, applyMiddleware, compose  } from 'redux';
 import thunkMiddleware from 'redux-thunk'
 import combinedReducers from './reducers/index';
 import { App } from './components/app';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   combinedReducers,
-  applyMiddleware(thunkMiddleware)
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
 );
 
 ReactDOM.render(
