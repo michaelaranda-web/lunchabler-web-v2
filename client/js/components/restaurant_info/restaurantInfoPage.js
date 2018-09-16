9914002fa685d470670b51ec25464a7ebe0e18e4
Michael Aranda <michaelaranda0.web@gmail.com>
2018-09-16 06:52:48 +0000

    Adds tab controls to the Restaurant Info page.

--- client/js/components/restaurant_info/restaurantInfoPage.js
+++ client/js/components/restaurant_info/restaurantInfoPage.js
@@ -6,19 +6,59 @@ import UserPreferences from './tabs/userPreferences';
 import Comments from './tabs/comments';
 
 export class RestaurantInfoPage extends React.Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      openTab: 'general-info'
+    }
+  }
+  
+  renderTabs() {
+    return (
+      <div id="tabs">
+        <div className="tab" onClick={ () => this.switchTab('general-info') }>
+          <i class="fas fa-align-left"></i>
+        </div>
+        <div className="tab" onClick={ () => this.switchTab('preferences') }>
+          <i class="far fa-grin-beam"></i>
+        </div>
+        <div className="tab" onClick={ () => this.switchTab('comments') }>
+          <i class="far fa-comment-dots"></i>
+        </div>
+      </div>
+    )
+  }
+  
+  renderOpenTab(restaurant) {
+    switch(this.state.openTab) {
+      case 'preferences':
+        return <UserPreferences restaurant={restaurant} />
+        break;
+      case 'comments':
+        return <Comments restaurant={restaurant} />
+        break;
+      default:
+        return <GeneralInfo restaurant={restaurant} />
+    }
+  }
+  
   render() {
     var restaurant = this.props.restaurantsById[this.props.match.params.restaurant_id];
     
     if (!!restaurant) {
       return (
         <div id="restaurant-info-page" className="page-content">
-          <GeneralInfo restaurant={restaurant} />
-          <UserPreferences restaurant={restaurant} />
-          <Comments restaurant={restaurant} />
+          {this.renderTabs()}
+          {this.renderOpenTab(restaurant)}
           <Link to="/results">Back to results</Link>
         </div>
       )
     } else { return null; }
+  }
+  
+  switchTab(tab) {
+    this.setState({openTab: tab})
   }
 }
 
