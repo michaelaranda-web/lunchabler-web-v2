bbb21874dd575c8caf896bb1a40c17d1e40afb06
Michael Aranda <michaelaranda0.web@gmail.com>
2018-11-20 08:43:34 +0000

    Adds placeholder activity panels to the Home Page.

--- client/js/components/home/homePage.js
+++ client/js/components/home/homePage.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 export class HomePage extends React.Component {
   render() {
-    return this.props.users.map((user) => {
-      return <p>{user.name}</p>
-    })
+    return (
+      <div id="home-page" className="page-content">
+        <div id="home-page-icon"></div>
+        <span>Instant Ink</span>
+        <div id="activity-panels">
+          <div id="notification-panel" className="activity-panel section">
+            <h4>SUGGESTION</h4>
+            Farmer's Market is today.
+          </div>
+          <div id="last-visit-panel" className="activity-panel section">
+            <h4>LAST VISIT</h4>
+            Jimbo's on Tuesday, Jan 23
+          </div>
+          <div id="recent-activity-panel" className="activity-panel section">
+            <h4>RECENT ACTIVITY</h4>
+            Derek said 'Meh' to Wahoo's Fish Tacos
+          </div>
+        </div>
+      </div>
+    )
   }
 }
+
+export default HomePage;
