c12bc1ea82cefa85c0c215eeb0ca850af570bfc3
Michael Aranda <michaelaranda0.web@gmail.com>
2018-12-20 09:33:51 +0000

    Add process.nextTick call to local-login strategy, which was in local-signup but not local-login. This might help with an intermittent issue where logging in caused an Invalid Status Code 1 server error.

--- server.js
+++ server.js
@@ -109,24 +109,26 @@ MongoClient.connect(db_url, function(err, client) {
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
     function(req, email, password, done) { // callback with email and password from our form
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
-      usersCol.findOne({ 'email': email }, function(err, user) {
-        // if there are any errors, return the error before anything else
-        if (err)
-            return done(err);
-
-        // if no user is found, return the message
-        if (!user)
-            return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-
-        // if the user is found but the password is wrong
-        if (!validPassword(password, user.password))
-            return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-
-        // all is well, return successful user
-        return done(null, user);
-      });
+      process.nextTick(function() {
+        // find a user whose email is the same as the forms email
+        // we are checking to see if the user trying to login already exists
+        usersCol.findOne({ 'email': email }, function(err, user) {
+          // if there are any errors, return the error before anything else
+          if (err)
+              return done(err);
+  
+          // if no user is found, return the message
+          if (!user)
+              return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+  
+          // if the user is found but the password is wrong
+          if (!validPassword(password, user.password))
+              return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+  
+          // all is well, return successful user
+          return done(null, user);
+        });
+      })
     }));
   
   passport.use('local-signup', new LocalStrategy({
