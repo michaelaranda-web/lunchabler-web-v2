4a298355418cb857c8f678ac11786203e4409b70
Michael Aranda <michaelaranda0.web@gmail.com>
2019-01-08 02:25:06 +0000

    Add ranking indicators to restaurant voe results on Voting Room page.

--- client/js/components/voting_room/scoreboardRow.js
+++ client/js/components/voting_room/scoreboardRow.js
@@ -14,12 +14,35 @@ export class ScoreboardRow extends React.Component {
     }
   }
   
+  renderRank() {
+    var rankClass;
+    
+    switch (this.props.rank) {
+      case 1:
+        rankClass = "gold top-result"
+        break;
+      case 2:
+        rankClass = "silver top-result"
+        break;
+      case 3:
+        rankClass = "bronze top-result"
+        break;
+      default:
+        rankClass = "";
+    }
+    
+    return (
+      <div className={`restaurant-rank ${rankClass}`}>{this.props.rank}</div>  
+    )
+  }
+  
   render() {
     const restaurant = this.props.restaurant
     
     if (restaurant && restaurant.yes.length > 0) {
       return (
         <div className="scoreboard-row section">
+          {this.renderRank()}
           <span className="restaurant-name">{restaurant.name}</span>
           <div className="restaurant-score-section">
             <i className='fas fa-thumbs-up'></i>
